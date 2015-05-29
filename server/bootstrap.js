6cb09454021b27cc8506929b64afef085e68b4a2
Oladayo Osikoya <alwaysdeone@gmail.com>
2015-05-29 19:20:42 +0100

    Remove previous collections, add dummy collection and data. Publish dummy data.

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,51 +1,28 @@
 // if the database is empty on server start, create some sample data.
 Meteor.startup(function () {
-  if (Lists.find().count() === 0) {
-    var data = [
-      {name: "Meteor Principles",
-       items: ["Data on the Wire",
-         "One Language",
-         "Database Everywhere",
-         "Latency Compensation",
-         "Full Stack Reactivity",
-         "Embrace the Ecosystem",
-         "Simplicity Equals Productivity"
-       ]
-      },
-      {name: "Languages",
-       items: ["Lisp",
-         "C",
-         "C++",
-         "Python",
-         "Ruby",
-         "JavaScript",
-         "Scala",
-         "Erlang",
-         "6502 Assembly"
-         ]
-      },
-      {name: "Favorite Scientists",
-       items: ["Ada Lovelace",
-         "Grace Hopper",
-         "Marie Curie",
-         "Carl Friedrich Gauss",
-         "Nikola Tesla",
-         "Claude Shannon"
-       ]
-      }
-    ];
+  if (Dummy.find().count() === 0) {
+    Dummy.insert({
+      name: "Dayo",
+      office: "iSpace",
+      title: "Code Monkey"
+    });
 
-    var timestamp = (new Date()).getTime();
-    _.each(data, function(list) {
-      var list_id = Lists.insert({name: list.name,
-        incompleteCount: list.items.length});
+    Dummy.insert({
+      name: "Lanre",
+      office: "iSpace",
+      title: "Solutions Architect"
+    });
 
-      _.each(list.items, function(text) {
-        Todos.insert({listId: list_id,
-                      text: text,
-                      createdAt: new Date(timestamp)});
-        timestamp += 1; // ensure unique timestamp.
-      });
+    Dummy.insert({
+      name: "Layi",
+      office: "iSpace",
+      title: "Hacker-at-large"
+    });
+
+    Dummy.insert({
+      name: "Fola",
+      office: "iSpace",
+      title: "Accountant"
     });
   }
 });
